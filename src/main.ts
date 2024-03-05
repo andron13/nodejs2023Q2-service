22da414690637bc77d92ac01c7c8c8cf77af0944
andron13 <andron13@gmail.com>
2024-03-05 15:09:33 +0100

    feat: start with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { PORT } from './share/config';
+import { swaggerConfig } from './share/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
 }
-bootstrap();
+void bootstrap();
---
+++ src/share/swagger.config.ts
@@ -0,0 +1,7 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('Evening Academy')
+  .setDescription('The Evening Academy API description')
+  .setVersion('1.0')
+  .build();
